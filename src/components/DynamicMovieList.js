9ea0714ac96078c1baade408fb19caf89b87eb97
ross-u <cirkovic.uros.srb@gmail.com>
2019-05-12 01:14:39 +0200

    Initial commit

---
+++ src/components/DynamicMovieList.js
@@ -0,0 +1,47 @@
+// src/components/DynamicMovieList.js
+import React, { Component } from 'react';
+import ImprovedCard from './ImprovedCard';
+
+class DynamicMovieList extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      movies: props.moviesArray,
+      showMovies: true
+    }
+  };
+
+  toggleMovies = () => {
+    this.setState({ showMovies: !this.state.showMovies });
+  }
+
+  deleteMovie = (movieIndex) => {
+    const moviesCopy = this.state.movies;
+    moviesCopy.splice(movieIndex, 1);
+    this.setState( {movies: moviesCopy} );
+  }
+
+  render() {
+    return (
+      <div>
+
+        <button onClick={this.toggleMovies}>
+          Toggle Movies
+        </button>
+
+        <ul>
+        {
+          this.state.showMovies ? 
+          this.state.movies.map( (oneMovie, index) => {
+            return <ImprovedCard key={index} {...oneMovie} clickToDelete={ ()=> this.deleteMovie(index)} />
+          })
+          :
+          null
+        }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default DynamicMovieList; 
