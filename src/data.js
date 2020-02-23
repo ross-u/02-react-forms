181d7ead8ca2d1a638a625e67f9cd89a0e1d9ca3
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-23 14:31:21 +0100

    Update mock data to include id

--- src/data.js
+++ src/data.js
@@ -1,10 +1,46 @@
 const data = [
-  { title: "The Godfather", director: "Francis Coppola", hasOscars: true, IMDbRating: 9.2 },
-  { title: "Star Wars", director: "Rian Johnson" , hasOscars: true, IMDbRating: 8.7 },
-  { title: "The Shawshank Redemption", director: "Frank Darabont", hasOscars: false, IMDbRating: 9.3 },
-  { title: "Jurassic Park", director: "Steven Spielberg", hasOscars: false, IMDbRating: 8.0  },
-  { title: "Sharknado", director: "Anthony C. Ferrante", hasOscars: false, IMDbRating: 5.2  },
-  { title: "Titanic", director: "James Cameron", hasOscars: true, IMDbRating: 8.9  }
-]
+  {
+    id: "3sdfg4",
+    title: "The Godfather",
+    director: "Francis Coppola",
+    hasOscars: true,
+    IMDbRating: 9.2
+  },
+  {
+    id: "2fgv34",
+    title: "Star Wars",
+    director: "Rian Johnson",
+    hasOscars: true,
+    IMDbRating: 8.7
+  },
+  {
+    id: "5sd61f",
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    hasOscars: false,
+    IMDbRating: 9.3
+  },
+  {
+    id: "g42d51",
+    title: "Jurassic Park",
+    director: "Steven Spielberg",
+    hasOscars: false,
+    IMDbRating: 8.0
+  },
+  {
+    id: "8eaf2f",
+    title: "Sharknado",
+    director: "Anthony C. Ferrante",
+    hasOscars: false,
+    IMDbRating: 5.2
+  },
+  {
+    id: "26f1e3",
+    title: "Titanic",
+    director: "James Cameron",
+    hasOscars: true,
+    IMDbRating: 8.9
+  }
+];
 
 export default data;
